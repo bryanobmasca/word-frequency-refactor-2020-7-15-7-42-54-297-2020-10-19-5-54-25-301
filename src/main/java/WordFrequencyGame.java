7f91221313edaa9947f30fe507818ecee5c87587
bryanobmasca <bryanobmasca@gmail.com>
2020-10-19 17:21:03 +0800

    [WordFrequency][Bryan] refactor: make WHITE_SPACES as local variable

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -2,8 +2,6 @@ import java.util.*;
 
 public class WordFrequencyGame {
 
-    private static final String WHITE_SPACES = "\\s+";
-
     public String getResult(String sentence) {
         List<WordInfo> wordInfoList = calculateWordFrequency(sentence);
         sortListByWordFrequency(wordInfoList);
@@ -25,6 +23,7 @@ public class WordFrequencyGame {
     }
 
     private List<WordInfo> calculateWordFrequency(String sentence) {
+        final String WHITE_SPACES = "\\s+";
         List<String> words = Arrays.asList(sentence.split(WHITE_SPACES));
         List<WordInfo> wordInfos = new ArrayList<>();
         for (String word : new HashSet<>(words)) {
