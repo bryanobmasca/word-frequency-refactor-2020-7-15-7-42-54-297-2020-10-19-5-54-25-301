d3e140df7c4f5e8e80d9491166a2e0facb73bf2e
bryanobmasca <bryanobmasca@gmail.com>
2020-10-19 16:00:13 +0800

    [WordFrequency][Bryan] refactor: change the old logic/approach into new method calculateWordFrequency

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -13,22 +13,7 @@ public class WordFrequencyGame {
 
             try {
 
-                String[] words = sentence.split(WHITE_SPACES);
-
-                List<WordInfo> wordInfoList = new ArrayList<>();
-                for (String word : words) {
-                    WordInfo wordInfo = new WordInfo(word, 1);
-                    wordInfoList.add(wordInfo);
-                }
-
-                Map<String, List<WordInfo>> wordInfoMap = getListMap(wordInfoList);
-
-                List<WordInfo> distinctWordInfos = new ArrayList<>();
-                for (Map.Entry<String, List<WordInfo>> entry : wordInfoMap.entrySet()){
-                    WordInfo wordInfo = new WordInfo(entry.getKey(), entry.getValue().size());
-                    distinctWordInfos.add(wordInfo);
-                }
-                wordInfoList = distinctWordInfos;
+                List<WordInfo> wordInfoList = calculateWordFrequency(sentence);
 
                 wordInfoList.sort((firstWord, secondWord) -> secondWord.getWordCount() - firstWord.getWordCount());
 
@@ -44,25 +29,13 @@ public class WordFrequencyGame {
         }
     }
 
-
-    private Map<String,List<WordInfo>> getListMap(List<WordInfo> wordInfoList) {
-        Map<String, List<WordInfo>> map = new HashMap<>();
-        for (WordInfo wordInfo : wordInfoList){
-//       map.computeIfAbsent(wordInfo.getValue(), k -> new ArrayList<>()).add(wordInfo);
-            if (!map.containsKey(wordInfo.getValue())){
-                ArrayList arr = new ArrayList<>();
-                arr.add(wordInfo);
-                map.put(wordInfo.getValue(), arr);
-            }
-
-            else {
-                map.get(wordInfo.getValue()).add(wordInfo);
-            }
+    private List<WordInfo> calculateWordFrequency(String sentence){
+        List<String> words = Arrays.asList(sentence.split(WHITE_SPACES));
+        List<WordInfo> wordInfos = new ArrayList<>();
+        for (String word : new HashSet<>(words)){
+            int frequency = Collections.frequency(words, word);
+            wordInfos.add(new WordInfo(word, frequency));
         }
-
-
-        return map;
+        return wordInfos;
     }
-
-
 }
